4cbea104b3347dad6ddb6de2c774fa59383ad14f
szymonkat <szymonkat@gmail.com>
2021-03-14 21:12:09 +0100

    first commit

---
+++ src/main/java/com/kodilla/transfersrealizationservice/service/TransferListener.java
@@ -0,0 +1,18 @@
+package com.kodilla.transfersrealizationservice.service;
+
+import com.kodilla.commons.TransferMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Slf4j
+@Service
+public class TransferListener {
+    @KafkaListener(topics = "transfers")
+    public void consume(@Payload TransferMessage transferMessage) throws IOException {
+        log.info("Consumed transferMessage: {}", transferMessage);
+    }
+}
